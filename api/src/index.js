39c84a370feb32491fa0a28137680725763bd0c3
Victor Yang <yanky2000@yahoo.com>
2020-06-18 22:26:44 +0300

    update nginx locations for web

--- api/src/index.js
+++ api/src/index.js
@@ -13,12 +13,17 @@ const Post = mongoose.model("Post", postSchema);
 
 app.get("/", (req, res) => res.send("start here with express"));
 app.get("/test", (req, res) => res.send("hello from express"));
+
 app.get("/api/currentUser", (req, resp) => {
     axios.get(authApiUrl + "/currentUser").then(res => {
         resp.json({ curUser: res.data });
     });
 });
-
+app.get("/user", (req, resp) => {
+    axios.get(authApiUrl + "/currentUser").then(res => {
+        resp.json({ curUser: res.data });
+    });
+});
 const startServer = () => {
     const post = new Post({ name: "first post ever!" });
     post.save(function (err, savedPost) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  // fetch('http://realworld-docker.com/api/posts').then( res => {
-  //   console.log(res)
-  // })
+    // fetch('http://realworld-docker.com/api/posts').then( res => {
+    //   console.log(res)
+    // })
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello from adf
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const makeReq = () => {
+        axios.get("/api/user").then(res => console.log(res.data));
+    };
+
+    return (
+        <div className="App">
+            {/* <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>hello from adf</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header> */}
+            <button onClick={makeReq}>Make req</button>
+        </div>
+    );
 }
 
 export default App;
